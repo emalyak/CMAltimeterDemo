11a0d8a89622d11a0564de7a92f95e6fba63008d
Erik Malyak <erik@skyhawkmedia.com>
2014-09-21 13:05:50 -0400

    Initial Commit

---
+++ CMAltimeterDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CMAltimeterDemo
+//
+//  Created by Erik Malyak on 9/21/14.
+//  Copyright (c) 2014 Erik Malyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
